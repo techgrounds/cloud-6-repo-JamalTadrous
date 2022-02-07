be4144560b531a1ea653a5fbd88dff14edc5fb96
JamalTadrous <95616021+JamalTadrous@users.noreply.github.com>
2022-02-07 13:30:02 +0100

    Initial commit

---
+++ PYTHON/PRG-02 - Variables.py
@@ -0,0 +1,30 @@
+# exercise 1
+
+x = 10
+y = 20
+Sum = (x + y)
+z = Sum
+print(z);
+
+# exercise 2
+
+name = "Jamal Tadrous"
+print("Hello, " + name);
+
+# exercice 3
+
+Value_1 = "Jamal"
+print("Value 1: " + Value_1)
+Value_1 = "Tadrous"
+
+Value_2 = 33
+print("Value 2: " + str(Value_2))
+Value_2 = 88
+
+Value_3 = "I think i get it"
+print("Value 3: " + Value_3)
+Value_3 = "Or maybe not?"
+
+print(Value_1)
+print(Value_2)
+print(Value_3)

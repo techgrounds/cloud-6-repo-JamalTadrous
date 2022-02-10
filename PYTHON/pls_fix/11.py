8746448d414c137cf0805cc07b22337b3d06aca6
JamalTadrous <95616021+JamalTadrous@users.noreply.github.com>
2022-02-10 13:15:45 +0100

    Initial commit

---
+++ PYTHON/pls_fix/11.py
@@ -0,0 +1,21 @@
+'''
+The output should be:
+IT LIVES!
+
+dev monster():
+	print('IT LIVES!')
+
+monster()
+'''
+
+def monster():
+	print('IT LIVES!')
+monster();
+
+# output: 
+# IT LIVES!
+
+
+# fixes:
+# dev -> def
+# monster() -> monster();

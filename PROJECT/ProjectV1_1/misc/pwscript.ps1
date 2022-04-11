cf856c186b1bbb197a0313c7f4a3add0f22c25a1
JamalTadrous <95616021+JamalTadrous@users.noreply.github.com>
2022-04-12 01:24:00 +0200

    My Commitment

--- PROJECT/ProjectV1_1/misc/pwscript.ps1
+++
@@ -1,9 +0,0 @@
-$templateFile = 'main.bicep'
-$deploymentName = "XYZv2"
-$Location = 'westeurope'
-
-New-AzSubscriptionDeployment `
--ResourceGroupName XYZv1 `
--Name $deploymentName `
--Location $Location `
--TemplateFile $templateFile
